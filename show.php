cc49920e69ac893b40fab7bb26146771d8e65a14
nely de faria <nelly.nf54@gmail.com>
2021-11-18 12:26:51 -0300

    Incluindo arquivo backbtn.html

--- show.php
+++ show.php
@@ -2,6 +2,7 @@
 require_once 'templates/header.php';
 ?>
   <div class="container" id="view-contact-container">
+    <?php require_once 'templates/backbtn.html'; ?>
     <h1 id="main-title"><?= $contact['name']?></h1>
     <p class="bold">Telefone:</p>
     <p><?= $contact['phone'] ?></p>
