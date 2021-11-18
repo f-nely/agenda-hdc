473866f281eae040b6a3dce2ddfc45e604b6df43
nely de faria <nelly.nf54@gmail.com>
2021-11-18 12:13:22 -0300

    Alterando propriedade href

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ require_once 'templates/header.php';
               <td scope="row"><?= $contact['name'] ?></td>
               <td scope="row"><?= $contact['phone'] ?></td>
               <td class="actions">
-                <a href="#"><i class="fas fa-eye check-icon"></i></a>
+                <a href="show.php?id=<?= $contact['id'] ?>"><i class="fas fa-eye check-icon"></i></a>
                 <a href="#"><i class="far fa-edit edit-icon"></i></a>
                 <button class="delete-btn" type="submit"><i class="fas fa-times delete-icon"></i></button>
               </td>
