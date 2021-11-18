9cf9aff1fa3771bfd463d1f3035820674a2ac189
nely de faria <nelly.nf54@gmail.com>
2021-11-18 14:03:26 -0300

    Criando funcionalidade de editar contatos

--- config/process.php
+++ config/process.php
@@ -28,14 +28,47 @@ if (!empty($data)) {
     $stmt->bindParam(':observations', $observations);
 
     try {
+
       $stmt->execute();
       $_SESSION['msg'] = 'Contato criado com sucesso!';
+
     } catch (PDOException $e) {
+
       $error = $e->getMessage();
       echo "Erro: $error";
+
+    }
+
+  } else if ($data['type'] === 'edit') {
+
+    $name = $data['name'];
+    $phone = $data['phone'];
+    $observations = $data['observations'];
+    $id = $data['id'];
+
+    $query = "UPDATE contacts 
+              SET name = :name, phone = :phone, observations = :observations 
+              WHERE id = :id";
+
+    $stmt = $conn->prepare($query);
+    $stmt->bindParam(':name', $name);
+    $stmt->bindParam(':phone', $phone);
+    $stmt->bindParam(':observations', $observations);
+    $stmt->bindParam(':id', $id);
+
+    try {
+
+      $stmt->execute();
+      $_SESSION['msg'] = 'Contato atualizado com sucesso!';
+
+    } catch (PDOException $e) {
+
+      $error = $e->getMessage();
+      echo "Erro: $error";
+
     }
   }
-
+ 
 // redirect HOME
 header('Location: ../index.php');
 
